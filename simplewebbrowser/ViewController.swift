191d95674cd0faed173265ea21c28c51d9df7e43
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-27 05:19:21 +0800

    Add ui and logic

--- simplewebbrowser/ViewController.swift
+++ simplewebbrowser/ViewController.swift
@@ -8,11 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UIWebViewDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var webView: UIWebView!
+    
+    @IBAction func back(_ sender: Any) {
+        if(webView.canGoBack){
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        if(webView.canGoForward){
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        var searchText = searchBar.text
+        
+        if(searchText?.lowercased().range(of: "https://") == nil || searchText?.lowercased().range(of: "http://") == nil){
+            searchText = "http://"+searchText!
+        }
+
+        if let url = URL(string: searchText!){
+            webView.loadRequest(URLRequest(url: url))
+        }else{
+            print("Error")
+        }
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView.loadRequest(URLRequest(url: URL(string: "https://www.google.com")!))
     }
 
     override func didReceiveMemoryWarning() {
